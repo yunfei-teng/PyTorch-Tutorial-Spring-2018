9dbcde2c3d167ef02df229e0f6fb80c65a39ba03
Yunfei Teng <yt1208@nyu.edu>
2018-02-28 15:25:08 -0500

    Update optim.py

--- classification/Architecture/optim.py
+++ classification/Architecture/optim.py
@@ -1,5 +1,5 @@
 # PyTorch tutorial codes for course EL-9133 Advanced Machine Learning, NYU, Spring 2018
-# Network/optim.py: define optimizer
+# Architecture/optim.py: define optimizer
 # read: http://pytorch.org/docs/master/optim.html
 import torch.optim as optim
 
@@ -13,6 +13,6 @@ print('batch size:',args.batch_size)
 print('seed number', args.seed)
 
 print('\n---Optimization Information---')
-print('pptimizer: SGD')
+print('optimizer: SGD')
 print('lr:', args.lr)
 print('momentum:', args.momentum)
